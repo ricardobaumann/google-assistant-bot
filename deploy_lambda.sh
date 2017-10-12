688ed9fe05b969eac4779766197ec453fc262cb0
Ricardo Baumann <gemini.richard@gmail.com>
2017-10-12 13:40:55 +0200

    Code cleanup

--- deploy_lambda.sh
+++ deploy_lambda.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-aws --region 'eu-central-1' s3 cp s3://up-config-eu-central-1/production/iroh/application.yml src/main/resources/application.yml
+aws --region 'eu-central-1' s3 cp s3://up-config-eu-central-1/production/content-bot/application.yml src/main/resources/application.yml
 ./gradlew clean build
 serverless deploy
