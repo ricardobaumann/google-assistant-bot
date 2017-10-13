68debc038f19d369d940be1d96f16689f8e84acb
Ricardo Baumann <gemini.richard@gmail.com>
2017-10-13 18:39:31 +0200

    lex sdk. it works

--- src/main/java/contentbot/AlexaLambdaHandler.java
+++ src/main/java/contentbot/AlexaLambdaHandler.java
@@ -3,15 +3,19 @@ package contentbot;
 import contentbot.domain.FulfillmentState;
 import contentbot.domain.LexRequest;
 import contentbot.domain.LexResponse;
+import contentbot.lambda.LexRequestStreamHandler;
 
 import java.util.Map;
 
 
-public class AlexaLambdaHandler extends AbstractLexRequestHandler {
+public class AlexaLambdaHandler extends LexRequestStreamHandler {
 
-
-    @Override
-    public LexResponse handleRequest(final LexRequest lexRequest, final Map<String, String> sessionAttributes) {
-        return createCloseDialogActionResponse(FulfillmentState.Failed, "Sorry, I'm having a problem fulfilling your request.  Please try again later.");
+    public AlexaLambdaHandler() {
+        super("WeltContentBot", new AbstractLexRequestHandler() {
+            @Override
+            public LexResponse handleRequest(final LexRequest lexRequest, final Map<String, String> sessionAttributes) {
+                return createCloseDialogActionResponse(FulfillmentState.Failed, "Sorry, I'm having a problem fulfilling your request.  Please try again later.");
+            }
+        });
     }
 }
